d7fe7ff00b27bea0201c07ebd21e89bb29e6c620
iamdanteonline <iamdanteonline@gmail.com>
2022-10-27 13:34:09 -0400

    pylint done

--- views/forms/products.py
+++ views/forms/products.py
@@ -1,9 +1,15 @@
+"""
+Products
+"""
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Length
 
 
 class CreateProductForm(FlaskForm):
+    """
+    Create Product
+    """
     name = StringField(
         label="Product name",
         name="product-name",
